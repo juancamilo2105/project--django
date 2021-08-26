d6d5ed3b8c1b1910bdb0588d2815a07e0e7ca060
Juan Camilo Trejo Bolaños <jctrejo53@misena.edu.co>
2021-08-25 20:36:40 -0500

    completado

--- home/views.py
+++ home/views.py
@@ -1,8 +1,40 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .models import *
+from .forms import agregar_producto_form
 
-def index(request):
-    return render(request, 'index.html')
+def vista_lista_producto (request):
+	lista = producto.objects.filter()
+	return render(request, 'lista_producto.html',locals())
 
-def listaproductos(request):
-    return HttpResponse('los productos son')
+def vista_agregar_producto(request):
+	if request.method == 'POST':
+		formulario = agregar_producto_form(request.POST, request.FILES)
+		if formulario.is_valid():
+			prod = formulario.save(commit = False)
+			prod.status = True
+			prod.save()
+			formulario.save_m2m()
+			return redirect ('/lista_producto/')
+	else:
+		formulario = agregar_producto_form()
+	return render(request, 'vista_agregar_producto.html', locals())
+
+def vista_ver_producto(request, id_prod):
+	p = producto.objects.get(id=id_prod)
+	return render(request, 'ver_producto.html',locals())
+
+def vista_editar_producto(request, id_prod):
+	prod = producto.objects.get(id=id_prod)
+	if request.method == 'POST':
+		formulario = agregar_producto_form(request.POST,request.FILES, instance=prod)
+		if formulario.is_valid():
+			prod = formulario.save()
+			return redirect ('/lista_producto/')
+	else:
+		formulario = agregar_producto_form(instance = prod)
+	return render(request, 'vista_agregar_producto.html', locals())
+
+def vista_eliminar_producto(request, id_prod):
+ 	prod = producto.objects.get(id=id_prod)
+ 	prod.delete()
+ 	return redirect ('/lista_producto/')	
--- tienda/settings.py
+++ tienda/settings.py
@@ -74,10 +74,20 @@ WSGI_APPLICATION = 'tienda.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+#'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+ 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'tienda',
+        'USER': 'root',
+        'PASSWORD': 'LAcasona',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
